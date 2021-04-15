4c17fd4386aa8d99d8c25802ae2718d39f267488
Yoelvis Mulen <ymulenll@gmail.com>
2021-04-15 18:33:13 -0300

    Añade menú superior

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".open-menu");
+const closeMenuBtn = document.querySelector(".close-menu");
+
+function toggleMenu() {
+  menu.classList.toggle("menu_opened");
+}
+
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu);
